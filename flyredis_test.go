184a2abe7ff2ffd39150022e43cc9cb69c298f44
kuhufu <kuhufu@gmail.com>
2019-06-26 16:09:21 +0800

    添加GET,SET,DEL方法的测试

--- flyredis_test.go
+++ flyredis_test.go
@@ -3,8 +3,11 @@ package flyredis
 import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
+	"log"
 	"testing"
 )
+
+var key = "flyredis_test_key"
 
 func TestNewPool(t *testing.T) {
 	p := NewPool(&redis.Pool{
@@ -17,4 +20,26 @@ func TestNewPool(t *testing.T) {
 	})
 
 	fmt.Println(p.KEYS("*forum*").Strings())
+}
+
+func TestSET(t *testing.T) {
+	if err := SET(key, "test_data").Error(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+// redis GET 当 key不存在时，将返回 nil, 也就是说 GET(key).Error() 和 GET(key).Reply() 都等于 nil
+// 而当进行类型转换时：res, err := GET(key).String(), res等于 nil, err不等于nil, error string 为：redigo: nil returned
+func TestGET(t *testing.T) {
+	if _, err := GET(key).String(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(GET(key).Value())
+}
+
+func TestDEL(t *testing.T) {
+	if reply, _ := DEL(key).Int(); reply == 0 {
+		log.Fatalf(`key "%v" not exist`, key)
+	}
 }
