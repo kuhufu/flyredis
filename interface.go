f9b793931737413b1dbb1b18aedeb0514366df7a
kuhufu <kuhufu@gmail.com>
2019-06-30 12:39:06 +0800

    将命令接口根据redis类型进行拆分

--- interface.go
+++ interface.go
@@ -1,23 +1,41 @@
 package flyredis
 
 type RedisInterface interface {
-	GET(args ...interface{}) Result
-	SET(args ...interface{}) Result
+	KeyCommand
+
+	StringCommand
+
+	HashCommand
+
+	SetCommand
+
+	Do(commandName string, args ...interface{}) Result
+	Send(commandName string, args ...interface{}) error
+}
+
+type KeyCommand interface {
 	DEL(args ...interface{}) Result
 	EXPIRE(args ...interface{}) Result
+	EXISTS(args ...interface{}) Result
 	KEYS(args ...interface{}) Result
+}
 
+type StringCommand interface {
+	GET(args ...interface{}) Result
+	SET(args ...interface{}) Result
+}
+
+type HashCommand interface {
 	HGET(args ...interface{}) Result
 	HSET(args ...interface{}) Result
+	HDEL(args ...interface{}) Result
 	HSETNX(args ...interface{}) Result
 	HGETALL(args ...interface{}) Result
 	HVALS(args ...interface{}) Result
 	HEXISTS(args ...interface{}) Result
-	HDEL(args ...interface{}) Result
+}
 
+type SetCommand interface {
 	SISMEMBER(args ...interface{}) Result
 	SADD(args ...interface{}) Result
-
-	Do(commandName string, args ...interface{}) Result
-	Send(commandName string, args ...interface{}) error
 }
