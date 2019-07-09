b81d2a465a2ac6ffb4b9eb5ad6b464a4578d5bce
kuhufu <kuhufu@gmail.com>
2019-07-09 17:43:09 +0800

    支持hash tag

--- cluster/cluster.go
+++ cluster/cluster.go
@@ -1,92 +1,99 @@
 package cluster
 
 import (
-	"github.com/gomodule/redigo/redis"
-	"github.com/kuhufu/flyredis"
 	"github.com/kuhufu/flyredis/crc16"
-	"strconv"
-	"time"
+	"strings"
 )
 
 const REDIS_CLUSTER_SLOTS = 16384
 
-type Addr struct {
-	IP   string
-	Port int
+type addr struct {
+	ip   string
+	port int
 }
 
-type ClusterNode struct {
-	FirstSlot int
-	LastSlot  int
+type node struct {
+	firstSlot int
+	lastSlot  int
 
-	Master Addr
-	Slaves []Addr
+	master addr
+	slaves []addr
 }
 
-func GetClusterSlotsInfo(reply []interface{}) []ClusterNode {
-	nodes := make([]ClusterNode, len(reply))
+func extractClusterSlotsInfo(reply []interface{}) []node {
+	nodes := make([]node, len(reply))
 	for i := 0; i < len(reply); i++ {
 
 		v := reply[i].([]interface{})
 
 		//master
 		master := v[2].([]interface{})
-		nodes[i] = ClusterNode{
-			FirstSlot: int(v[0].(int64)),
-			LastSlot:  int(v[1].(int64)),
-			Master: Addr{
-				IP:   string(master[0].([]uint8)),
-				Port: int(master[1].(int64)),
+		nodes[i] = node{
+			firstSlot: int(v[0].(int64)),
+			lastSlot:  int(v[1].(int64)),
+			master: addr{
+				ip:   string(master[0].([]uint8)),
+				port: int(master[1].(int64)),
 			},
 		}
 
-		var slaves []Addr
+		var slaves []addr
 		//slave
 		for i := 3; i < len(v); i++ {
 			slave := v[i].([]interface{})
-			slaves = append(slaves, Addr{
-				IP:   string(slave[0].([]uint8)),
-				Port: int(slave[1].(int64)),
+			slaves = append(slaves, addr{
+				ip:   string(slave[0].([]uint8)),
+				port: int(slave[1].(int64)),
 			})
 		}
 
-		nodes[i].Slaves = slaves
+		nodes[i].slaves = slaves
 	}
 	return nodes
 }
 
 func SlotNumber(key string) int {
+	//支持 hash tag
+	l := strings.Index(key, "{")
+	r := strings.Index(key, "}")
+
+	if l != -1 && r != -1 {
+		key = key[l+1 : r]
+	}
 	return int(crc16.Checksum([]byte(key)) % REDIS_CLUSTER_SLOTS)
 }
 
-func NewClient(proc, addr string, options ...redis.DialOption) *ClusterClient {
-	conn, err := flyredis.Dial(proc, addr, options...)
-	defer conn.Close()
-	if err != nil {
-		return nil
+func nodesEqual(a, b []node) bool {
+	if len(a) != len(b) {
+		return false
 	}
 
-	reply, err := conn.Do("cluster", "slots").Values()
-	if err != nil {
-		return nil
+	for i := 0; i < len(a); i++ {
+		if !nodeEqual(a[i], b[i]) {
+			return false
+		}
 	}
-	nodes := GetClusterSlotsInfo(reply)
+	return true
+}
 
-	pools := make([]*flyredis.Pool, len(nodes))
+func nodeEqual(a, b node) bool {
+	if a.firstSlot != b.firstSlot || a.lastSlot != b.lastSlot || a.master != b.master || len(a.slaves) != len(b.slaves) {
+		return false
+	}
+	for i := 0; i < len(a.slaves); i++ {
+		if a.slaves[i] != b.slaves[i] {
+			return false
+		}
+	}
+	return true
+}
+
+func sortNodes(nodes []node) {
 	for i := 0; i < len(nodes); i++ {
-		addr := nodes[i].Master.IP + ":" + strconv.Itoa(nodes[i].Master.Port)
-		pools[i] = flyredis.NewPool(&redis.Pool{
-			MaxIdle:     50,
-			MaxActive:   1000,
-			IdleTimeout: 50 * time.Second,
-			Dial: func() (conn redis.Conn, err error) {
-				return redis.Dial("tcp", addr)
-			},
-		})
-	}
-
-	return &ClusterClient{
-		nodes: nodes,
-		pools: pools,
+		for j := i + 1; j < len(nodes); j++ {
+			if nodes[i].firstSlot > nodes[j].firstSlot {
+				nodes[i], nodes[j] = nodes[j], nodes[i]
+			}
+		}
 	}
 }
