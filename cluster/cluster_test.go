567321ab3ba1a24d3e7c2b026715d0ec68cdc486
kuhufu <kuhufu@gmail.com>
2019-07-09 17:43:27 +0800

    更新测试代码

--- cluster/cluster_test.go
+++ cluster/cluster_test.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
 	"github.com/kuhufu/flyredis"
+	"regexp"
+	"strings"
 	"testing"
 	"time"
 )
@@ -19,11 +21,11 @@ var pool = flyredis.NewPool(&redis.Pool{
 
 func TestGetClusterSlotsInfo(t *testing.T) {
 	reply, _ := pool.Do("cluster", "slots").Values()
-	fmt.Println(reply)
+	fmt.Println(extractClusterSlotsInfo(reply))
 }
 
 func TestSlotNumber(t *testing.T) {
-	key := "k2"
+	key := "{k2}:key"
 	slotNum, err := pool.Do("CLUSTER", "KEYSLOT", key).Int()
 	if err != nil {
 		t.Fatal(err)
@@ -36,4 +38,99 @@ func TestSlotNumber(t *testing.T) {
 func TestGet(t *testing.T) {
 	fmt.Println(pool.GET("k1").String())
 	fmt.Println(pool.GET("k2").String())
+}
+
+func TestNodeEqual(t *testing.T) {
+	tests := []struct {
+		a    node
+		b    node
+		want bool
+	}{
+		{node{}, node{}, true},
+		{node{firstSlot: 1}, node{}, false},
+		{node{slaves: []addr{}}, node{}, true},
+	}
+	for _, test := range tests {
+		if nodeEqual(test.a, test.b) != test.want {
+			t.Error("err")
+		}
+	}
+}
+
+var r, _ = regexp.Compile(`{(.+?)}`)
+
+func TestHashTag(t *testing.T) {
+	tests := []struct {
+		key  string
+		want string
+	}{
+		{key: "{user}:key", want: "user"},
+		{key: "{user:key", want: "{user:key"},
+		{key: "user}:key", want: "user}:key"},
+		{key: "{user}:{key}", want: "user"},
+	}
+	for _, test := range tests {
+		key := test.key
+		if r.MatchString(key) {
+			key = strings.Trim(r.FindString(key), "{}")
+		}
+		if key != test.want {
+			t.Error("in:", test.key, "out:", key, "want:", test.want)
+		}
+	}
+}
+
+func TestHashTag3(t *testing.T) {
+	key := `{user}:key`
+	a := strings.Index(key, "{")
+	c := strings.Index(key, "}")
+
+	if a != -1 && c != -1 {
+		key = key[a+1 : c]
+	}
+
+	tests := []struct {
+		key  string
+		want string
+	}{
+		{key: "{user}:key", want: "user"},
+		{key: "{user:key", want: "{user:key"},
+		{key: "user}:key", want: "user}:key"},
+		{key: "{user}:{key}", want: "user"},
+	}
+	for _, test := range tests {
+		key := test.key
+		a := strings.Index(key, "{")
+		c := strings.Index(key, "}")
+		if a != -1 && c != -1 {
+			key = key[a+1 : c]
+		}
+		if key != test.want {
+			t.Error("in:", test.key, "out:", key, "want:", test.want)
+		}
+	}
+}
+
+//BenchmarkFoo-8    	30000000	        60.4 ns/op
+//BenchmarkFoo3-8   	100000000	        13.6 ns/op
+func BenchmarkFoo(b *testing.B) {
+	key := `{user}:key`
+	for i := 0; i < b.N; i++ {
+		if r.MatchString(key) {
+			key = strings.Trim(r.FindString(key), "{}")
+		}
+	}
+}
+
+func BenchmarkFoo3(b *testing.B) {
+	key := `{user}:key`
+	for i := 0; i < b.N; i++ {
+		a := strings.Index(key, "{")
+		c := strings.Index(key, "}")
+
+		if a != -1 && c != -1 {
+			key = key[a+1 : c]
+		}
+	}
+
 }
