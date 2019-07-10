2a4d4fe42a5e42e78691e4006ab2d359bb4ee7c0
kuhufu <kuhufu@gmail.com>
2019-07-10 23:29:31 +0800

    更新测试

--- cluster/cluster_client_test.go
+++ cluster/cluster_client_test.go
@@ -3,17 +3,50 @@ package cluster
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"testing"
-	"time"
 )
 
-func TestClusterClient_Do(t *testing.T) {
-	var client, err = NewClient("tcp", "127.0.0.1:7000")
+func TestClient_Do_SET(t *testing.T) {
+	c, err := NewClient("tcp", "127.0.0.1:7000")
 	if err != nil {
-		log.Fatal(err)
+		log.Println(err)
 		return
 	}
 
-	fmt.Println(client)
-	time.Sleep(time.Hour)
+	for i := 0; i < 50000; i++ {
+		if err := c.Do("SET", strconv.Itoa(i), i).Error(); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestClient_Do_GET(t *testing.T) {
+	c, err := NewClient("tcp", "127.0.0.1:7000")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for i := 0; i < 50000; i++ {
+		if res, _ := c.Do("GET", strconv.Itoa(i)).Int(); res != i {
+			t.Error()
+		}
+	}
+}
+
+func TestClient_Do_Race(t *testing.T) {
+	c, err := NewClient("tcp", "127.0.0.1:7000")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	for i := 0; i < 50000; i++ {
+		i := i
+		go func() {
+			c.Do("GET", strconv.Itoa(i))
+		}()
+		fmt.Println("finished")
+	}
 }
